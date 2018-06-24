d550b1e96abb0c580241973dbf35081d4d6f2fbf
Matt Marshall <matt.marshallian@gmail.com>
2018-06-24 11:10:36 +0200

    Główna pętla while i obsługa eventów

--- SDL Basic/SDL Basic.cpp
+++ SDL Basic/SDL Basic.cpp
@@ -16,7 +16,7 @@ using namespace std;
 int main()
 {
 	const int WINDOW_WIDTH = 800;
-	const int WINDOW_HEIGTH = 600;
+	const int WINDOW_HEIGHT = 600;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cout << "SDL screwed up" << endl;
@@ -26,11 +26,24 @@ int main()
 	cout << "Hello world with SDL2" << endl;
 
 	SDL_Window *window = SDL_CreateWindow("Particle Explosion Fire",
-		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGTH, SDL_WINDOW_SHOWN);
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
 	if (nullptr == window) {
 		SDL_Quit();
 		return 2;
+	}
+
+	SDL_Event event;
+	bool quit = false;
+	while (!quit) { // ta pętla żre cały jeden rdzeń procesora
+		// Upgrade particles
+		// Draw particles
+		// Check for messages/events
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		};
 	}
 
 	SDL_Quit();
