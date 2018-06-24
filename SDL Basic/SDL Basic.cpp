f3d6451eb7973cd4cecd0f996cc38e0aa5130c67
Matt Marshall <matt.marshallian@gmail.com>
2018-06-24 10:51:16 +0200

    Utworzenie okna

--- SDL Basic/SDL Basic.cpp
+++ SDL Basic/SDL Basic.cpp
@@ -15,12 +15,23 @@ using namespace std;
 
 int main()
 {
+	const int WINDOW_WIDTH = 800;
+	const int WINDOW_HEIGTH = 600;
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cout << "SDL screwed up" << endl;
 		return 1;
 	}
 
 	cout << "Hello world with SDL2" << endl;
+
+	SDL_Window *window = SDL_CreateWindow("Particle Explosion Fire",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGTH, SDL_WINDOW_SHOWN);
+
+	if (nullptr == window) {
+		SDL_Quit();
+		return 2;
+	}
 
 	SDL_Quit();
 
