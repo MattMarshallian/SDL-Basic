28d1af9b91d7faba6d4f0f5535b87e45d44df1e4
Matt Marshall <matt.marshallian@gmail.com>
2018-07-03 15:04:49 +0200

    Cząsteczki odbijają się od krawędzi

--- SDL Basic/Particle.cpp
+++ SDL Basic/Particle.cpp
@@ -7,8 +7,8 @@ Particle::Particle()
 {
 	m_x = 2.0 * rand() / RAND_MAX - 1.0;
 	m_y = 2.0 * rand() / RAND_MAX - 1.0;
-	m_xspeed = 0.003 * (((2.0 * rand()) / RAND_MAX) - 1);
-	m_yspeed = 0.004 * (((2.0 * rand()) / RAND_MAX) - 1);
+	m_xspeed = 0.002 * (((2.0 * rand()) / RAND_MAX) - 1);
+	m_yspeed = 0.002 * (((2.0 * rand()) / RAND_MAX) - 1);
 }
 
 
@@ -21,4 +21,11 @@ void Particle::update()
 {
 	m_x += m_xspeed;
 	m_y += m_yspeed;
+
+	if (m_x <= -1.0 || m_x >= 1.0)
+		m_xspeed = -m_xspeed;
+
+	if (m_y <= -1.0 || m_y >= 1.0)
+		m_yspeed = -m_yspeed;
+
 }
