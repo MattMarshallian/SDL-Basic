31bb0971418874323c3719ba0acc94d72df1dcf9
Matt Marshall <matt.marshallian@gmail.com>
2018-07-03 07:12:20 +0200

    Płynna zmiana koloru całego ekranu

--- SDL Basic/SDL Basic.cpp
+++ SDL Basic/SDL Basic.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <math.h>
 #include "Screen.h"
 
 // #undef ponizej to jakas sztuczka z
@@ -22,11 +23,16 @@ int main()
 
 	while (true) { // ta pętla żre cały jeden rdzeń procesora
 		// Updade particles
+		int elapsed = SDL_GetTicks();
+
+		unsigned int red = static_cast<unsigned char>((1 + sin(elapsed * 0.0001)) * 128);
+		unsigned int green = static_cast<unsigned char>((1 + sin(elapsed * 0.0002)) * 128);
+		unsigned int blue = static_cast<unsigned char>((1 + sin(elapsed * 0.0003)) * 128);
 
 		// Draw particles
 		for (int y = 0; y < Screen::SCREEN_HEIGHT; ++y) {
 			for (int x = 0; x < Screen::SCREEN_WIDTH; ++x) {
-				screen.setPixel(x, y, 128, 0, 255);
+				screen.setPixel(x, y, red, green, blue);
 			}
 		}
 		screen.setPixel(400, 300, 255, 255, 255);
