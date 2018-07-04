419fb3da5475e9433496ca6f7262a601bdabf75e
Matt Marshall <matt.marshallian@gmail.com>
2018-07-04 06:41:56 +0200

    Niezamierzony efekt odbić a implozja i tak pozostała.

--- SDL Basic/Particle.cpp
+++ SDL Basic/Particle.cpp
@@ -10,10 +10,7 @@
 
 Particle::Particle() :m_x(0), m_y(0)
 {
-	m_direction = (2.0 * M_PI * rand()) / RAND_MAX;
-	m_speed = (0.02 * rand()) / RAND_MAX;
-
-	m_speed *= m_speed;
+	init();
 }
 
 
@@ -27,6 +24,18 @@ void Particle::update(int interval)
 	m_direction += 0.0004 * interval;
 	double xspeed = m_speed * cos(m_direction);
 	double yspeed = m_speed * sin(m_direction);
+
 	m_x += xspeed * interval;
 	m_y += yspeed * interval;
+	if (m_x <= -1 || m_x >= 1 || m_y <= -1 || m_y >= 1) {
+		init();
+	}
+}
+
+void Particle::init()
+{
+	m_direction = (2.0 * M_PI * rand()) / RAND_MAX;
+	m_speed = (0.02 * rand()) / RAND_MAX;
+
+	m_speed *= m_speed;
 }
--- SDL Basic/Particle.h
+++ SDL Basic/Particle.h
@@ -3,15 +3,18 @@ class Particle
 {
 	// intentionally changed access to public
 public:
-	double m_x;
-	double m_y;
-	double m_direction;
-	double m_speed;
+	double m_x = 0;
+	double m_y = 0;
 
 
 public:
 	Particle();
 	virtual ~Particle();
 	void update(int interval);
+
+private:
+	double m_direction;
+	double m_speed;
+	void init();
 };
 
